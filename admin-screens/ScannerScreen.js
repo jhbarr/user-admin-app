5f15e4aec44f2fa762ae2a568fde3039897c62ae
Sean0z <143973083+Sean0z@users.noreply.github.com>
2024-07-12 20:33:31 -0700

    Rudimentary QR Code Scanner

--- admin-screens/ScannerScreen.js
+++ admin-screens/ScannerScreen.js
@@ -1,9 +1,46 @@
 import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { CameraView, Camera } from 'expo-camera';
 
 export default function ScannerScreen({ navigation }) {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [scannedData, setScannedData] = useState('');
+
+  useEffect(() => {
+    const getCameraPermissions = async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    };
+
+    getCameraPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setScannedData(data);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Scanner will go here</Text>
+      <CameraView
+        onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}
+        barcodeScannerSettings={{
+          barcodeTypes: ["qr", "pdf417"],
+        }}
+        style={styles.camera}
+      />
+      {scanned && (
+        <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />
+      )}
+      <Text style={styles.scannedDataText}>"QR Code Data: "{scannedData}</Text>
       <Button title="Add Rewards" onPress={() => navigation.navigate("Add")} />
       <Button title="Back" onPress={() => navigation.navigate("Home")} />
     </View>
@@ -13,7 +50,7 @@ export default function ScannerScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    flexDirection: "column",
     justifyContent: 'center',
   },
   text: {
@@ -21,4 +58,28 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginBottom: 16,
   },
+  camera: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 500,
+    width: 375,
+    overflow: 'hidden',
+    borderRadius: 30,
+    backgroundColor: 'tomato'
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  scannedDataText: {
+    fontSize: 16,
+    marginBottom: 10,
+  }
 });
